904755c9f3324175c51d131954e8aa617221a943
M'hamed HLIOUI <hmrocket@gmail.com>
2019-06-05 13:44:30 -0400

    add ApiUtil verifySign method and its test

--- app/src/main/java/com/jumboprivacy/api/ApiUtil.kt
+++ app/src/main/java/com/jumboprivacy/api/ApiUtil.kt
@@ -1,8 +1,13 @@
 package com.jumboprivacy.api
 
+import android.util.Base64
 import java.math.BigInteger
 import java.nio.charset.StandardCharsets
+import java.security.KeyFactory
 import java.security.MessageDigest
+import java.security.PublicKey
+import java.security.Signature
+import java.security.spec.X509EncodedKeySpec
 
 object ApiUtil {
 
@@ -15,5 +20,28 @@ object ApiUtil {
         md.update(publicKey.trim().toByteArray(StandardCharsets.UTF_8))
         val digest = md.digest()
         return String.format("%064x", BigInteger(1, digest))
+    }
+
+    fun verifySign(publicKey: String, content: String, signature: String): Boolean {
+        val dsa = Signature.getInstance("SHA256withRSA")
+        dsa.initVerify(publicKey(publicKey))
+
+        dsa.update(content.toByteArray())
+        val sign = Base64.decode(
+            signature,
+            Base64.DEFAULT
+        )
+
+        return dsa.verify(sign)
+    }
+
+    /**
+     * generate PublicKey from string
+     */
+    fun publicKey(publicKey: String): PublicKey {
+        val keyFac = KeyFactory.getInstance("RSA")
+        val keySpec = X509EncodedKeySpec(Base64.decode(publicKey.trim { it <= ' ' }.toByteArray(), Base64.DEFAULT))
+        val key = keyFac.generatePublic(keySpec)
+        return key
     }
 }
--- app/src/test/java/com/jumboprivacy/api/ApiUtilTest.kt
+++ app/src/test/java/com/jumboprivacy/api/ApiUtilTest.kt
@@ -17,4 +17,20 @@ class ApiUtilTest {
         val repo = "b6e60cb831003b2d51bfac3288160ad13e7a407e6be0f2cec8c3e32a303857bd"
         assertEquals(hex, repo)
     }
+
+//    @Test
+//    fun signature() {
+//        val publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvj/vNM+fcg1EsqUNVpWt" +
+//                "0R5vHS1KpSOSYN0oBUzcnKNXREuFQAWxhopx6cf4/F0TnP+P9XkDYpDIVvyybeoJ" +
+//                "lxKcgXClitKKqsYI45GHIhoUdYkE8a4u4qKJkPqvev7mY0Yoa6pASTjCroOjIHCY" +
+//                "EwuPy1Tm0TqIL99x4DMJnTgi5VFd580iHb4kzSlkt10sLKqYiJ8w4rjB2sG6FXqW" +
+//                "27hnpiWxWUkGIqkF+UahAwEx+dcqKf3z9y8V/VpKDbBQNq8fvGcawyHgIBBaB0+C" +
+//                "AdaTJbBY15pn27+f5XRxZja21Gof3+TLMzj/vsiwaQ4IHklj5VEDU65Rc4jz+C0m" +
+//                "IQIDAQAB"
+//
+//        val sign = "Mcc5CwRh670YN8HUCbxVlsbg+Y89BoFQgJ0rIn6DTEp8Jz6zM7YBYOAbVRZQ0UwDZDHqOsFnmDZmOGPNfzp2fdQaqc30h0S8NZiGtOsSw9DKFDCRc9kBoReccDi6Q4WRxKq0vsPfXJnDyz6xArZHKAdADRuRoSSu8li9yrVb9WYOyZ55rGqbPyI9m8fcTzvK7JlHwp+gtzTCOiRf0J3aGdgiPa3+xX+WRGHLt847LnwYylqIpNWHuFvlEdVqNpGd9BAOqFc+MrDXlXWAFu3IDWKEYsE8C1bFx4qLPpXOl4vTS/NulNTTXpUU1fDtcgBw3nIaQ76BZ+RWAZkJFBn/pg=="
+//
+//        Assert.assertTrue(ApiUtil.verifySign(publicKey, "Hello", sign))
+//
+//    }
 }
